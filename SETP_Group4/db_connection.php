c1bc345f5c273c5fb932455f5d1ecd636a1f3676
PungJL <59461655+JLPung@users.noreply.github.com>
2021-07-09 01:10:28 +0800

    Update db_connection.php

--- SETP_Group4/db_connection.php
+++ SETP_Group4/db_connection.php
@@ -1,21 +1,12 @@
 <?php
 
 function OpenCon(){
- $dbhost = "sql6.freesqldatabase.com:330";
+ $dbhost = "sql6.freesqldatabase.com:3306";
  $dbuser = "sql6423581";
  $dbpass = "zjlFur9zEL";
  $db = "sql6423581";
 
 
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db)
 
- 
- return $conn;
- }
- 
-function CloseCon($conn)
- {
- $conn -> close();
- }
-   
 ?>
