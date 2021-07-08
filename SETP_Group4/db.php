1eabb194e15bcbfc27f041694328738782268d6d
PungJL <59461655+JLPung@users.noreply.github.com>
2021-07-08 23:20:47 +0800

    Update db.php
    used for include php functions for later purposes only.

--- SETP_Group4/db.php
+++ SETP_Group4/db.php
@@ -1,3 +1,4 @@
+/* This is will use for refactoring. include 'db.php'; */
 <?php
 $con = mysqli_connect('sql6.freesqldatabase.com:3306','sql6423581','zjlFur9zEL');
 mysqli_select_db($con,'sql6423581');
