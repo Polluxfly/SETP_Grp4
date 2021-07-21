750f27da1e880574c2de2acf70374f70cf4ee21a
eshf <62289445+eshf@users.noreply.github.com>
2021-07-22 00:47:52 +0800

    Add files via upload

--- SETP_Group4/profile.php
+++ SETP_Group4/profile.php
@@ -2,13 +2,67 @@
 
 session_start();
 
+$con = mysqli_connect('sql6.freesqldatabase.com:3306','sql6423581','zjlFur9zEL');
+
+mysqli_select_db($con,'sql6423581');
+error_reporting(0);
+
+$userid = "";
+$role = "";
+$status = "";
+$username = "";
+$email = "";
+$password = "";
+$gender = "";
+$nationality = "";
+$mobile = "";
+$dob = "";
+$email2 = "";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+//connect to mysql database
+try{
+    $con = mysqli_connect('sql6.freesqldatabase.com:3306','sql6423581','zjlFur9zEL');
+}catch(mysqli_Sql_Exception $ex)
+{
+    echo("error in connecting");
+}
+//get data from the form
+function getData()
+{
+    $data = array();
+    $data[0]=$_POST['userid'];
+    $data[1]=$_POST['role'];
+    $data[2]=$_POST['status'];
+    $data[3]=$_POST['username'];
+    $data[4]=$_POST['email'];
+    $data[5]=$_POST['password'];
+    $data[6]=$_POST['gender'];
+    $data[7]=$_POST['nationality'];
+    $data[8]=$_POST['mobile'];
+    $data[9]=$_POST['dob'];
+    $data[10]=$_POST['email2'];
+    return $data;
+}
+
+mysqli_select_db($con,'sql6423581');
+
+
+
+
+
+
 ?>
 
-
 <html>
+<style>
+body {
+  background-image: url("normalpage_back.jpg");
+}
+</style>
 <head>
 
-    <title>Milo Bing</title>
+    <title>OH, Hi YO !</title>
     <link href="css/profile.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="Bootstrap/bootstrap.css">
 </head>
@@ -16,82 +70,168 @@ session_start();
 
 <body>
         <a class="logout" href="logout.php"> LOGOUT </a>
-        <div class="companylogo"><img src="img/logo.png" alt="logo"><h1>Milo Bing Language Centre</h1></div>
+        <div class="companylogo"><img src="logo.png" alt="logo"></div>
         
         <div class="navigation">
             <ul>
                 <li><a href="homepage.php">Home Page</a></li>
                 <li><a href="createuser.php">Registration</a></li>
-                <li><a href="profile.php">Student Profile</a></li>
+                <li><a href="profile.php">User Profile</a></li>
                 <li><a href="#">Course Details</a></li>
-                <li><a href="#">Enrollment</a></li>
+                <li><a href="studentpaymentpage.php">Enrollment</a></li>
                 <li><a href="#">Student Progress</a></li>
-                <li><a href="#">Report</a></li>
+                <li><a href="enquiry.php">Enquiries</a></li>
 
             </ul>
         </div>
-         
+         <div class="messagebox"><h3>
         <?php
 
-        $con = mysqli_connect('localhost','root','','lms');
-        if($con->connect_error){
-        die("Connection failed". $con->connect_error);  
+            //Update
+
+if(isset($_POST['update'])){
+$info = getData();
+$update_query ="UPDATE `userinfo` SET role='$info[1]', status='$info[2]',username='$info[3]',email='$info[4]',password='$info[5]',gender='$info[6]',nationality='$info[7]',mobile='$info[8]',dob='$info[9]' WHERE userid = '$info[0]'";
+try{
+    $update_result=mysqli_query($con,$update_query);
+    if($update_result){
+     if(mysqli_affected_rows($con)>0)
+     {
+      echo("Details Updated !");
+	 }else{
+      echo("Update Failed !");
+	 }
+	}
+}catch(Exception $ex){
+    echo("Error In Update".$ex->getMessage());
+}
+}
+
+//search
+if(isset($_POST['search']))
+{   
+    $info = getData();
+    $search_query = "SELECT * FROM `userinfo` WHERE (userid='$info[0]' or username LIKE '%$info[3]%') ";
+    $search_result = mysqli_query($con,$search_query);
+    
+        if($search_result)
+        {
+            if(mysqli_num_rows($search_result))
+            {
+                while($rows = mysqli_fetch_array($search_result))
+                {
+                    $userid = $rows['userid'];
+                    $role = $rows['role'];
+                    $status = $rows['status'];
+                    $username = $rows['username'];
+                    $email = $rows['email'];
+                    $password = $rows['password'];
+                    $gender = $rows['gender'];
+                    $nationality = $rows['nationality'];
+                    $mobile = $rows['mobile'];
+                    $dob = $rows['dob'];
+                    $email2 = $rows['email'];
+                    
+                   
+
+				}
+			}else{
+                    echo("Student Not Found !");     
+			}
+		} else {
+                echo("Error");  
 		}
+}
 
-        $result = mysqli_query($con,"SELECT * FROM userinfo");
-        ?>
-
-        <?php
-
-        if(mysqli_num_rows($result)>0){
-
-        ?>
-        <div class="wrap">
-        <table>
-        <tr>
-        <th>Student ID</th>
-        <th>Student Status</th>
-        <th>Student Name</th>
-        <th>Gender</th>
-        <th>Nationality</th>
-        <th>Email</th>
-        <th>Mobile</th>
-        <th>Date of Birth</th>
-        <th>Action</th>
-        </tr>
-
-        <?php
-        $i=0;
-          while ($row = mysqli_fetch_array($result)){
-
-          ?>
-          <tr>
-          <td><?php echo $row["userid"];?></td>
-          <td><?php echo $row["status"];?></td>
-          <td><?php echo $row["username"];?></td>
-          <td><?php echo $row["gender"];?></td>
-          <td><?php echo $row["nationality"];?></td>
-          <td><?php echo $row["email"];?></td>
-          <td><?php echo $row["mobile"];?></td>
-          <td><?php echo $row["dob"];?></td>
-          <td><a href="updateprofile.php?userid=<?php echo $row["userid"];?>">Update</a></td>
-          </tr>
-          <?php
-          $i++;
-          }
-          ?>
+//delete
+if(isset($_POST['delete']))
+{
+    $info = getData();
+    $delete_query = "DELETE FROM `userinfo` WHERE userid = '$info[0]' and role='Student'";
+    try{
+     $delete_result= mysqli_query($con,$delete_query);
+     if($delete_result){
+      if(mysqli_affected_rows($con)>0)
+      {
+       echo("Student Deleted !");
+	  }else{
+       echo("Data Not Deleted");
+	  }
+	 }
+	}catch(Exception $ex){
+     echo ("Error in Delete".$ex ->getMessage());
+	}
+}
 
 
-        </table></div>
-        <?php
 
-        }
-        else {
-        echo "No result found";
-        }
-        ?>
-        
+            ?></h3></div>
+       
+       <div class="container">
+       <div class="search-box">
+       <div class="row">
+       <div class="col-md-6 update-left">
+       <form method="post" action="profile.php">
+       <h1> Search </h1>
+            <div class="form-group">
+            <input type="number" name="userid" class="form-control" placeholder="Student ID" value="<?php echo ($userid);?>"><br>
+            <input type="email" name="email2" class="form-control" placeholder="Student Email" value="<?php echo ($email2);?>"><br>
+            </div>
 
-    </body>
+            <button class="btn4" name="search">Search</button><br>
 
-    </html>
+            </div>
+
+
+            <div class="col-md-6 update-right">
+            <h1> User Details </h1>
+            <div class="form-group">
+            <select id="role" name="role">
+                            <option><?php echo ($role);?></option>
+                            <option value="Admin">Admin</option>
+                            <option value="Student">Student</option>
+                            </select>
+            </div><br>
+            <div class="form-group">
+            <select id="status" name="status">
+                            <option><?php echo ($status);?></option>
+                            <option value="Active">Active</option>
+                            <option value="Inactive">Inactive</option>
+                            </select>
+            </div><br>
+
+            <div class="form-group">
+            <input type="text" name="username" class="form-control" placeholder="User Name" value="<?php echo ($username);?>"><br>
+            </div>
+            <div class="form-group">
+            <input type="text" name="email" class="form-control" placeholder="User Email" value="<?php echo ($email);?>"><br>
+            </div>
+           <div class="form-group">
+            <input type="text" name="password" class="form-control" placeholder="User Password" value="<?php echo ($password);?>"><br>
+            </div>
+            <div class="form-group">
+            <input type="text" name="gender" class="form-control" placeholder="User Gender" value="<?php echo ($gender);?>"><br>
+            </div>
+            <div class="form-group">
+            <input type="text" name="nationality" class="form-control" placeholder="User Nationality" value="<?php echo ($nationality);?>"><br>
+            </div>
+            <div class="form-group">
+            <input type="text" name="mobile" class="form-control" placeholder="User Mobile Number" value="<?php echo ($mobile);?>"><br>
+            </div>
+            <div class="form-group">
+            <input type="date" name="dob" class="form-control" placeholder="User Date of Birth" value="<?php echo ($dob);?>"><br>
+            </div>
+
+            <button class="btn2" name="update">Update</button><br><br>
+            
+            <button class="btn3" name="delete">Delete</button>
+            
+            
+             
+       </from>
+       </div>
+       </div>
+       </div>
+       </div>
+       
+</html>
