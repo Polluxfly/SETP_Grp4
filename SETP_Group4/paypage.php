84f80c23dc11cb503fbc70de6646454e0d82312a
PungJL <59461655+JLPung@users.noreply.github.com>
2021-07-23 01:07:48 +0800

    Update paypage.php

--- SETP_Group4/paypage.php
+++ SETP_Group4/paypage.php
@@ -20,7 +20,7 @@ body {
         <div class="companylogo"><img src="logo.png" alt="logo"></div>
         <div class="navigation">
             <ul>
-                <li><a href="payment.php">Back to confirmation</a></li>
+                <li><a href="enrollment.php">Back to confirmation</a></li>
                 
 
             </ul>
@@ -33,31 +33,36 @@ body {
         <h3 style="text-align: center;">Choose a payment for your "Course"</h3>
        </span>
        </div>
-
-	<div class="PaymentContainer">
-        <img src="visa_master.png" alt="visa_master_logo">
-        <form action="charge.php" method="post" id="payment-form">
-        <div class="form-row">
-        <input type="text" name="first_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="First Name">
-        <input type="text" name="last_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Last Name">
-        <input type="email" name="email" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Email Address">
-        <div id="card-element" class="form-control">
-          <!-- a Stripe Element will be inserted here. -->
-        </div>
-        <!-- Used to display form errors -->
-        <div id="card-errors" role="alert"></div>
-      </div>
-      <button>Submit Payment</button>
-    </form>
-       
-    </div>
-    <div class="apple-payment-request-button">
-      <img src="button-pay-with-apple.png" alt="button-pay-with-apple-logo">
-    </div>
-
-    <div id="googlepay-button" class="googlepay-button">
-    </div>
-         
+	
+	<div class="container" style="display: flex; position: absolute; top: 35%; left: 15%">
+		<div class="row">
+			<div class="col-sm">
+				<div class="apple-payment-request-button" style="display: flex; width: 29px; padding: 30px; ">
+				<img src="button-pay-with-apple.png" alt="button-pay-with-apple-logo">
+			</div>
+			<div class="col-sm">
+				<div id="googlepay-button" class="googlepay-button" style="display: flex; width: 29px; height: 50px; padding: 30px"></div>
+			</div>
+			<div class="col-sm">
+				<div class="PaymentContainer" style="width: 390px; padding: 10px; top: 10%">
+					<img src="visa_master.png" alt="visa_master_logo" style="width: 130px;">
+					<form action="charge.php" method="post" id="payment-form">
+						<div class="form-row">
+						<input type="text" name="first_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="First Name">
+						<input type="text" name="last_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Last Name">
+						<input type="email" name="email" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="Email Address">
+						<div id="card-element" class="form-control">
+						</div>
+						<!-- Used to display form errors -->
+						<div id="card-errors" role="alert"></div>
+						</div>
+						<button style="display: flex;">Submit Payment</button>
+					</form>
+				</div>
+			</div>
+			</div>
+		</div> 
+	</div> 
         <script async src="https://pay.google.com/gp/p/js/pay.js" onload="onGooglePayLoaded()"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://js.stripe.com/v3/"></script>
