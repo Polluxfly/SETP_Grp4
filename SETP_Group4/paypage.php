fd4fba3fc3e3ac58fa1a887489f363298f0616b8
PungJL <59461655+JLPung@users.noreply.github.com>
2021-07-24 21:07:25 +0800

    Update paypage.php

--- SETP_Group4/paypage.php
+++ SETP_Group4/paypage.php
@@ -34,18 +34,11 @@ body {
        </span>
        </div>
 	
-	<div class="container" style="display: flex; position: absolute; top: 35%; left: 15%">
+	<div class="container" style="display: flex; position: absolute; top: 35%;">
 		<div class="row">
-			<div class="col-sm">
-				<div class="apple-payment-request-button" style="display: flex; width: 29px; padding: 30px; ">
-				<img src="button-pay-with-apple.png" alt="button-pay-with-apple-logo">
-			</div>
-			<div class="col-sm">
-				<div id="googlepay-button" class="googlepay-button" style="display: flex; width: 29px; height: 50px; padding: 30px"></div>
-			</div>
-			<div class="col-sm">
-				<div class="PaymentContainer" style="width: 390px; padding: 10px; top: 10%">
-					<img src="visa_master.png" alt="visa_master_logo" style="width: 130px;">
+			<!--div class="col-sm"-->
+				<div class="PaymentContainer" style="width: 690px; padding: 10px; top: 10%">
+					<img src="visa_master.png" alt="visa_master_logo" style="width: 150px;">
 					<form action="charge.php" method="post" id="payment-form">
 						<div class="form-row">
 						<input type="text" name="first_name" class="form-control mb-3 StripeElement StripeElement--empty" placeholder="First Name">
@@ -58,16 +51,14 @@ body {
 						</div>
 						<button style="display: flex; btn btn-primary btn-sm">Submit Payment</button>
 					</form>
-				</div>
+				<!--/div-->
 			</div>
 			</div>
 		</div> 
 	</div> 
-        <script async src="https://pay.google.com/gp/p/js/pay.js" onload="onGooglePayLoaded()"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://js.stripe.com/v3/"></script>
         <script src="./js/charge.js"></script>
-        <script src="./js/googlepay.js"></script>
          
 </body>
 </html>
