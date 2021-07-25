e663a33aef7f4b5fd645f9f0646264e27e0fdb4f
PungJL <59461655+JLPung@users.noreply.github.com>
2021-07-25 17:40:02 +0800

    Update script.js

--- SETP_Group4/js/script.js
+++ SETP_Group4/js/script.js
@@ -4,10 +4,7 @@ $(function() {
     var cardNumber = $('#cardNumber');
     var cardNumberField = $('#card-number-field');
     var CVV = $("#cvv");
-    var mastercard = $("#mastercard");
     var confirmButton = $('#confirm-purchase');
-    var visa = $("#visa");
-    var amex = $("#amex");
 
     // Use the payform library to format and validate
     // the payment fields.
@@ -15,43 +12,15 @@ $(function() {
     cardNumber.payform('formatCardNumber');
     CVV.payform('formatCardCVC');
 
-
-    cardNumber.keyup(function() {
-
-        amex.removeClass('transparent');
-        visa.removeClass('transparent');
-        mastercard.removeClass('transparent');
-
-        if ($.payform.validateCardNumber(cardNumber.val()) == false) {
-            cardNumberField.addClass('has-error');
-        } else {
-            cardNumberField.removeClass('has-error');
-            cardNumberField.addClass('has-success');
-        }
-
-        if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
-            mastercard.addClass('transparent');
-            amex.addClass('transparent');
-        } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
-            mastercard.addClass('transparent');
-            visa.addClass('transparent');
-        } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
-            amex.addClass('transparent');
-            visa.addClass('transparent');
-        }
-    });
-
     confirmButton.click(function(e) {
 
         e.preventDefault();
-
+		
         var isCardValid = $.payform.validateCardNumber(cardNumber.val());
         var isCvvValid = $.payform.validateCardCVC(CVV.val());
 
         if(owner.val().length < 5){
 			xdialog.alert("Wrong owner name");
-        } else if (!isCardValid) {
-            xdialog.alert("Wrong card number");
         } else if (!isCvvValid) {
             xdialog.alert("Wrong CVV");
         } else {
