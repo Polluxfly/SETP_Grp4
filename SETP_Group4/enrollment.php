17303c92ff9443060fc9e386ac1b31e26b5d1770
PungJL <59461655+JLPung@users.noreply.github.com>
2021-07-25 13:44:58 +0800

    Update enrollment.php

--- SETP_Group4/enrollment.php
+++ SETP_Group4/enrollment.php
@@ -25,7 +25,7 @@
    while($row=$result->fetch_assoc())
    {
    $user_name = $row['username'];
-   
+		
    }
    }
    
@@ -54,7 +54,7 @@
       </div>
       <div class="container" style="position: absolute; top: 45%; left: 12%;">
          <div class="row">
-            <div class="col-sm-4">
+            <div class="col-sm-6">
                <div class="">
                   <span>
                      <h1>Enroll in a course now ! <?php echo $user_name; ?></h1>
@@ -70,8 +70,7 @@
                      		  ON e.courseid = c.courseid
                      		  INNER JOIN userinfo AS u
                      		  ON e.userid = u.userid
-                     		  WHERE c.status = '1' AND u.status = 'Active' AND u.userid = '$loginuser'";
-                     
+                     		  WHERE c.status = '1' AND u.status = 'Active' AND u.userid = '$loginuser' ORDER BY paystatus";
                      $result = mysqli_query($con, $SQL) or die('A error occured: ');
                      $number_of_rows = mysqli_num_rows($result);
                      	echo '<table class="table-scroll small-first-col">';
@@ -81,6 +80,8 @@
                      	echo '  <th>Course ID </th>';
                      	echo '  <th>Course Level </th>';
                      	echo '  <th>Course Fee </th>';
+						echo '	<th>Payment Status</th>';
+						echo '<th>Action</th>';
                      echo '</thead>';
                      	echo '</tr>';
                      
@@ -90,44 +91,32 @@
                      			echo '<td>' . $Row['courseid'] . '</td>';
                      			echo '<td>' . $Row['courselevel'] . '</td>';
                      			echo '<td>' . '$' .$Row['coursefee'] . '</td>';
-                              echo '</tr>';
+								if($Row['paystatus'] == 0)
+									{
+									echo "<td> Not Yet</td>";
+									}
+									else
+									{
+									echo "<td> Paid</td>";
+									}
+								if ($Row['paystatus'] == '0')
+								    { 
+								    $id = $Row['enrollmentid'];
+								    echo "<td><button class=\"btn btn-primary btn-sm\" style=\"right: 50px\"  onclick=\"onclickRedirect()\">Pay</button></td>";
+								    } 
+								    else
+								    { 
+								    echo"<td>  </td>";
+								   }
+                                echo '</tr>';
                            }
                         echo '</table>'
                      ?>
                </div>
             </div>
-            <div class="col-sm-2">
-               <div class="">
-                  <table class="table-scroll small-first-col">
-                  <thead>
-                     <tr>
-                        <th>Payment Status</th>
-                     </tr>
-                  </thead>
-                  <?php
-                     $SQL = 	" SELECT *
-                                    		  FROM enrollmentinfo AS e
-                                    		  INNER JOIN courseinfo AS c 
-                                    		  ON e.courseid = c.courseid
-                                    		  INNER JOIN userinfo AS u
-                                    		  ON e.userid = u.userid
-                                    		  WHERE c.status = '1' AND u.status = 'Active' AND u.userid = '$loginuser'";
-                                    
-                                    $result = mysqli_query($con, $SQL) or die('A error occured: ');
-                                    $number_of_rows = mysqli_num_rows($result);
-                                             while ($Row = mysqli_fetch_assoc($result)) {
-                                             if ($Row['paystatus'] == '0')
-                                    	         { 
-                                                $id = $Row['enrollmentid'];
-                                                echo "<tr><td><button class=\"btn btn-primary btn-sm\" style=\"right: 50px\"  onclick=\"onclickRedirect()\">Pay</button></td></tr>";
-                                    			   } 
-                                    			   else
-                                    			   { 
-                                    		       echo"<tr><td><a href=\"#\" class=\"btn btn-success btn-sm\" style=\"right: 50px\" >Paid</a></td></tr>";
-                                    			   }
-                                          }
-                                       echo '</table>'
-                                    ?>                    
+            
+                                            
+                 
                </div>
             </div>
          </div>
